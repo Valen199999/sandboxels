dcf253ec61374035ebee2daf830bd94c7d3214fe
Valen199999 <scratchvalen@gmail.com>
2024-06-23 22:17:42 -0300

    Add files via upload

---
+++ mods/Oganeson.js
@@ -0,0 +1,9 @@
+elements.oganesson = {
+	color: "#CDA25C",
+	behavior: behaviors.STURDYPOWDER,
+	category: "ValenObjects",
+	state: "solid",
+	reactions: {
+        "radiation": { elem1: oganesson, elem2: "water" },
+    },
+};
